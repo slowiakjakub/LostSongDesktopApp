a528b861ec4e572d0668f4dbc02bfef787b2127c
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-16 01:37:44 +0100

    Add renaming file functionality

---
+++ LostSongDesktopAppBackend/DataAccess/FileProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostSongDesktopAppBackend.DataAccess
+{
+    public static class FileProcessor
+    {
+        public static void RenameFile(string sourceFilePath, string finalFileName)
+        {
+            FileInfo fi = new FileInfo(sourceFilePath);
+            if (fi.Exists)
+            {
+                fi.MoveTo($@"{fi.Directory.FullName}\{finalFileName}.mp3");
+            }
+        }
+    }
+}
