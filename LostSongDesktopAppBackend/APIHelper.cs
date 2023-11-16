001d40f8a096906e91cf3147c485879bf392899d
Jakub Slowiak <slowiakjakub@gmail.com>
2023-11-16 01:38:23 +0100

    Interact with API

---
+++ LostSongDesktopAppBackend/APIHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostSongDesktopAppBackend
+{
+    public class APIHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            APIHelper.SetApplicationClientKey();
+
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public static void SetApplicationClientKey()
+        {
+            AcoustID.Configuration.ClientKey = "pYEBEoFX0R";
+        }
+    }
+}
